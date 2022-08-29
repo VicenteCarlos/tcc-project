d779db53d06c38fad34ceae5f25d78669f5030f4
vicentecarlos <soniamariadias2012@hotmail.com>
2022-08-29 12:15:08 -0300

    refactor: reverse order by section

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,17 +8,6 @@ const Login = () => {
   return (
     <MainStyled>
       <motion.section
-        variants={variants_1}
-        initial="hidden"
-        animate="visible"
-        className="imgLeft"
-      >
-        <img
-          src="https://cdn.discordapp.com/attachments/1008067136872255560/1011985227222110249/Group_56071.png"
-          alt="img: Transforme o futuro do seu dinheiro."
-        />
-      </motion.section>
-      <motion.section
         className="containerForm"
         variants={variants_2}
         initial="hidden"
@@ -30,6 +19,17 @@ const Login = () => {
             Não possui uma conta? <Link to="/register">Registre-se</Link>
           </motion.span>
         </div>
+      </motion.section>
+      <motion.section
+        variants={variants_1}
+        initial="hidden"
+        animate="visible"
+        className="imgLeft"
+      >
+        <img
+          src="https://cdn.discordapp.com/attachments/1008067136872255560/1011985227222110249/Group_56071.png"
+          alt="img: Transforme o futuro do seu dinheiro."
+        />
       </motion.section>
     </MainStyled>
   );
