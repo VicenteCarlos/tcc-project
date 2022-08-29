24a4179aa0f75651106b0adefea9fab7b167a23b
vicentecarlos <soniamariadias2012@hotmail.com>
2022-08-29 12:30:07 -0300

    more space for test

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -26,6 +26,7 @@ const Login = () => {
         animate="visible"
         className="imgLeft"
       >
+       
         
         <img
           src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
